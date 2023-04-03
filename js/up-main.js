225d4a6cc4016801563ba2b034c363cdb1a3dd5b
sconnell <sconnell@nfb.org>
2023-04-03 16:55:08 -0400

    Initial commit

---
+++ js/up-main.js
@@ -0,0 +1,8 @@
+(function ($, Drupal) {
+    Drupal.behaviors.coms_request_wash = {
+        attach: function (context, settings) {
+            window.onload = function () {
+            }
+        }
+    }
+    })(jQuery, Drupal)
