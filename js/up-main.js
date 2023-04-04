38c6930889ee95478c999e4e51a04663c0415312
sconnell <sconnell@nfb.org>
2023-04-04 10:16:18 -0400

    Initial commit

--- js/up-main.js
+++ js/up-main.js
@@ -6,3 +6,16 @@
         }
     }
     })(jQuery, Drupal)
+function show_hide_edit_feild(div_id)
+{
+    var disp = document.getElementById(div_id).style.display;
+    if(disp == "None")
+    {
+        document.getElementById(div_id).style.display = "Block";
+    }
+    else
+    {
+        document.getElementById(div_id).style.display = "None";
+    }
+}
+
