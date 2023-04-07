5accac4a0bc27455a868b8ae51f0586a01a7d92f
sconnell <sconnell@nfb.org>
2023-04-07 14:43:49 -0400

    Initial commit

--- src/html_buider/core_markup.php
+++ src/html_buider/core_markup.php
@@ -1,12 +1,43 @@
 <?php
 Namespace Drupal\nfb_user_portal\html_builder;
+use Drupal\nfb_user_portal\user\user_civi;
+
 class core_markup
 {
     public $user_data; // user civi class object
     public $core_markup; //string
+    public function __construct()
+    {
+        $this->user_data = new user_civi();
+    }
     public function get_core_markup()
     {
         return $this->core_markup;
+    }
+    public function create_core_markup()
+    {
+        $this->user_data->set_civi_useR_data(); //set all data
+
+    }
+    public function header_2(){
+        $markup = "
+<p>Intro text oges here.</p>
+<p class='hidden_val' id='user_id_val'>".$this->user_data->get_user_id()."</p>
+<p class='hidden_val' id = 'civi_id_val'>".$this->user_data->get_user_civi_id()."</p>
+<p class='hidden_val' id = 'user_name_val'>".$this->user_data->get_user_name()."</p>
+<p class='hidden_val' id='edit_open'>Not Open</p>
+                   <p class='hidden_val' id='open_field'>None</p>
+                    <h2>Your Contact Information</h2>
+                    <p id='f_name'>First Name: ".$this->user_data->get_first_name()." &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a role='button' aria-label='Edit first Name' id='edit_f_name'>&nbsp;&nbsp;&nbsp;Edit&nbsp;&nbsp;&nbsp;</a></p>
+                    <div id='first_name_edit_div'><form><imput id='f_name_new_val' class= 'feild_custom_text'aria-label='enter new first name></imput></form>&nbsp;&nbsp;&nbsp;<a role='button' id='cancel_f_name'>Cancel</a><a role='button' id='save_f_name'>Save</a></div>
+                    <p id='l_name'>Last Name: ".$this->user_data->get_last_name()." &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a role='button' aria-label='Edit Last Name' id='edit_l_name'>&nbsp;&nbsp;&nbsp;Edit&nbsp;&nbsp;&nbsp;</a></p>
+                    <div id='last_name_edit_div'><form><imput id='l_name_new_val' class= 'feild_custom_text' aria-label='enter new last name'></imput></form>&nbsp;&nbsp;&nbsp;<a role='button' id='cancel_l_name'>Cancel</a><a role='button' id='save_l_name'>Save</a></div>
+                    <p id='prime_email'>Email: ".$this->user_data->get_prime_email()." &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a role='button' aria-label='Edit Primary Email' id='edit_prime_email'>&nbsp;&nbsp;&nbsp;Edit&nbsp;&nbsp;&nbsp;</a></p>
+                    <div id='prim_email_edit_div'><form><imput id='prim_email_new_val' class= 'feild_custom_text' aria-label='enter new primary email'></imput></form>&nbsp;&nbsp;&nbsp;<a role='button' id='cancel_prim_email'>Cancel</a><a role='button' id='save_prim_email'>Save</a></div>
+                    <p id='prime_phone'>Phone: ".$this->user_data->get_prime_phone()." &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a role='button' aria-label='Edit Primary Email' id='edit_prime_email'>&nbsp;&nbsp;&nbsp;Edit&nbsp;&nbsp;&nbsp;</a></p>
+                    <div id='prim_phone_edit_div'><form><imput id='prim_phone_new_val' class= 'feild_custom_text' aria-label='enter new primary phone'></imput></form>&nbsp;&nbsp;&nbsp;<a role='button' id='cancel_prim_phone'>Cancel</a><a role='button' id='save_prim_phone'>Save</a></div>
+                    ";
+
     }
 
 
