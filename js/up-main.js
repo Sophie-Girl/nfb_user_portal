26752273a5eaa0fe9d5806f8ea6debd7894fb2b3
sconnell <sconnell@nfb.org>
2023-04-10 10:51:16 -0400

    Initial commit

--- js/up-main.js
+++ js/up-main.js
@@ -19,8 +19,153 @@
                     ajax_change_call(vfeildarray);
                 }
             });
+            $('#save-prim-email').once().onclick(function ()
+            {
+                var vfeildarray;
+                var feild_data = document.getElementById('prim_phone_new_val').value;
+                if(feild_data == "")
+                {
+                    vfeildarray = "not_run";
+                }
+                else {
+                    vfeildarray[0] = "email";
+                    vfeildarray[1] =  document.getElementById('civi_id_val').innerText;
+                    vfeildarray[2] = document.getElementById('prim_email_new_val').value
+                    ajax_change_call(vfeildarray);
+                }
+            });
+            $('#save-f_name').once().onclick(function ()
+            {
+                var vfeildarray;
+                var feild_data = document.getElementById('f_name_new_val').value;
+                if(feild_data == "")
+                {
+                    vfeildarray = "not_run";
+                }
+                else {
+                    vfeildarray[0] = "f_name";
+                    vfeildarray[1] =  document.getElementById('civi_id_val').innerText;
+                    vfeildarray[2] = document.getElementById('f_name_new_val').value
+                    ajax_change_call(vfeildarray);
+                }
+            });
+            $('#save-l-name').once().onclick(function ()
+            {
+                var vfeildarray;
+                var feild_data = document.getElementById('l_name_new_val').value;
+                if(feild_data == "")
+                {
+                    vfeildarray = "not_run";
+                }
+                else {
+                    vfeildarray[0] = "l_name";
+                    vfeildarray[1] =  document.getElementById('civi_id_val').innerText;
+                    vfeildarray[2] = document.getElementById('l_name_new_val').value
+                    ajax_change_call(vfeildarray);
+                }
+            });
+            $('#edit_f_name').once().onclick(function ()
+            {
+              var editstatus = document.getElementById("edit_open").innerText;
+              var openfeild = document.getElementById("open_field").innerText;
+              if(editstatus == "Not Open")
+              {
+                  document.getElementById("first_name_edit_div").style.display = "Block";
+                  document.getElementById("edit_open").innerText = "Open";
+                  document.getElementById("open_field").innerText = "First Name";
+              }
+              else if( editstatus == "Open" && openfeild == "First Name")
+              {
+                  document.getElementById("first_name_edit_div").style.display = "None";
+                  document.getElementById("edit_open").innerText = "Not Open";
+                  document.getElementById("open_field").innerText = "None";
+              }
+              else{
+                  alert("Cannot edit more than one field at a time. Please close the "+ openfeild +" edit field and try again.")
+              }
+            });
+            $('#edit_l_name').once().onclick(function ()
+            {
+                var editstatus = document.getElementById("edit_open").innerText;
+                var openfeild = document.getElementById("open_field").innerText;
+                if(editstatus == "Not Open")
+                {
+                    document.getElementById("last_name_edit_div").style.display = "Block";
+                    document.getElementById("edit_open").innerText = "Open";
+                    document.getElementById("open_field").innerText = "Last Name";
+                }
+                else if( editstatus == "Open" && openfeild == "Last Name")
+                {
+                    document.getElementById("last_name_edit_div").style.display = "None";
+                    document.getElementById("edit_open").innerText = "Not Open";
+                    document.getElementById("open_field").innerText = "None";
+                }
+                else{
+                    alert("Cannot edit more than one field at a time. Please close the "+ openfeild +" edit field and try again.")
+                }
+            });
+            $('#edit_prim_email').once().onclick(function ()
+            {
+                var editstatus = document.getElementById("edit_open").innerText;
+                var openfeild = document.getElementById("open_field").innerText;
+                if(editstatus == "Not Open")
+                {
+                    document.getElementById("prim_email_edit_div").style.display = "Block";
+                    document.getElementById("edit_open").innerText = "Open";
+                    document.getElementById("open_field").innerText = "Prime Email";
+                }
+                else if( editstatus == "Open" && openfeild == "Prime Email")
+                {
+                    document.getElementById("prim_email_edit_div").style.display = "None";
+                    document.getElementById("edit_open").innerText = "Not Open";
+                    document.getElementById("open_field").innerText = "None";
+                }
+                else{
+                    alert("Cannot edit more than one field at a time. Please close the "+ openfeild +" edit field and try again.")
+                }
+            });
+            $('#edit_prim_phone').once().onclick(function ()
+            {
+                var editstatus = document.getElementById("edit_open").innerText;
+                var openfeild = document.getElementById("open_field").innerText;
+                if(editstatus == "Not Open")
+                {
+                    document.getElementById("prim_phone_edit_div").style.display = "Block";
+                    document.getElementById("edit_open").innerText = "Open";
+                    document.getElementById("open_field").innerText = "Prime Phone";
+                }
+                else if( editstatus == "Open" && openfeild == "Prime Phone")
+                {
+                    document.getElementById("prim_phone_edit_div").style.display = "None";
+                    document.getElementById("edit_open").innerText = "Not Open";
+                    document.getElementById("open_field").innerText = "None";
 
-
+                }
+                else{
+                    alert("Cannot edit more than one field at a time. Please close the "+ openfeild +" edit field and try again.")
+                }                    document.getElementById("edit_open").innerText = "Not Open";
+                document.getElementById("open_field").innerText = "None";
+            });
+            $('#cancel_prim_phone').once().onclick(function () {
+                document.getElementById("prim_phone_edit_div").style.display = "None";
+                document.getElementById("edit_open").innerText = "Not Open";
+                document.getElementById("open_field").innerText = "None";
+            });
+            $('#cancel_prim_email').once().onclick(function () {
+                document.getElementById("prim_email_edit_div").style.display = "None";
+                document.getElementById("edit_open").innerText = "Not Open";
+                document.getElementById("open_field").innerText = "None";
+            });
+            $('#cancel_last_name').once().onclick(function () {
+                document.getElementById("last_name_edit_div").style.display = "None";
+                document.getElementById("edit_open").innerText = "Not Open";
+                document.getElementById("open_field").innerText = "None";
+            });
+            $('#cancel_first_name').once().onclick(function () {
+                document.getElementById("first_name_edit_div").style.display = "None";
+                document.getElementById("edit_open").innerText = "Not Open";
+                document.getElementById("open_field").innerText = "None";
+            });
         }
 
     }
@@ -29,7 +174,7 @@
         if(vefeildarray == "no-run"){
         $.ajax({
             type: 'POST',
-            url: '/nfb_washington/admin/ajax/committee',
+            url: '/nfb_member_portal/ajax/change',
             data: { feildarray:vfeildarray },
         }).done(function (data) {});
     }
