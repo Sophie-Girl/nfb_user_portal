d411336c4adc4f345b97fd8437cc7635373b9e3d
sconnell <sconnell@nfb.org>
2023-04-25 08:12:47 -0400

    Initial commit

--- src/html_builder/core_markup.php
+++ src/html_builder/core_markup.php
@@ -23,7 +23,7 @@ class core_markup
     public function header_2(){
         $markup = "
 <p>Welcome to your National Federation of the Blind member profile. Achieving our shared goals of equality, opportunity, and security for the blind are impossible without you as member. In your account, maintain your personal information and review membership status. The member profile is under construction and expanding; you’ll find several tabs to different areas of the member profile. Review information in your profile, membership, FAQs, and more. Thank you for your active participation in the organized blind movement.
-<br>
+<br> <br>
 To update your contact or demographic information, navigate to the appropriate field and select Edit. Enter the correct information (or select it from the menu) and Save. Once you select Edit, you must Cancel or Save before editing another field.</p>
 <p class='hidden_val' id='user_id_val'>".\Drupal::currentUser()->getAccount()->id()."</p>
 <p class='hidden_val' id = 'civi_id_val'>".$this->user_data->get_user_civi_id()."</p>
