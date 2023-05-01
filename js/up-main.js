847fcd96cd1306d85463c9e47925753a541e7920
sconnell <sconnell@nfb.org>
2023-05-01 15:21:40 -0400

    Initial commit

--- js/up-main.js
+++ js/up-main.js
@@ -683,7 +683,7 @@
                 if (editstatus == "Not Open") {
                     document.getElementById("disability_edit_div").style.display = "Block";
                     document.getElementById("disability_new_val").style.display = "Block";
-                    document.getElementById("cancel_disability").style.display = "block";
+                    document.getElementById("cancel_disability").style.display = "inline-block";
                     document.getElementById("save_disability").style.display = "inline-block";
                     document.getElementById("edit_open").innerText = "Open";
                     document.getElementById("open_field").innerText = "disability";
