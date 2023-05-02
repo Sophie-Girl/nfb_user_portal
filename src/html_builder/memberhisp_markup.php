73636afe05262163a9d84079d5b8df837906e6a7
sconnell <sconnell@nfb.org>
2023-05-02 16:47:31 -0400

    Initial commit

--- src/html_builder/memberhisp_markup.php
+++ src/html_builder/memberhisp_markup.php
@@ -4,5 +4,9 @@ use Drupal\nfb_user_portal\civi_query\query_base;
 use Drupal\nfb_user_portal\user\user_membership;
 class memberhisp_markup
 {
-
+    public $markup;
+    public function get_markup()
+    {
+        return $this->markup;
+    }
 }
