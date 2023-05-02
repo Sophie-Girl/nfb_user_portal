bad8f7d55ae3d8ae18a347f7e5e409b697a53883
sconnell <sconnell@nfb.org>
2023-05-02 14:39:34 -0400

    Initial commit

---
+++ src/html_builder/memberhisp_markup.php
@@ -0,0 +1,8 @@
+<?php
+Namespace Drupal\nfb_user_portal\html_builder;
+use Drupal\nfb_user_portal\civi_query\query_base;
+use Drupal\nfb_user_portal\user\user_membership;
+class memberhisp_markup
+{
+
+}
