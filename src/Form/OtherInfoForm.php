e87fbadcf4511d257051d208c44c4db6522a9021
sconnell <sconnell@nfb.org>
2023-05-16 14:48:28 -0400

    UX nonsense

---
+++ src/Form/OtherInfoForm.php
@@ -0,0 +1,19 @@
+<?php
+Namespace Drupal\nfb_user_portal\Form;
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+class OtherInfoForm extends FormBase
+{
+    public function getFormId()
+    {
+       return "member_portal_other_info";
+    }
+    public function buildForm(array $form, FormStateInterface $form_state)
+    {
+        // TODO: Implement buildForm() method.
+    }
+    public function submitForm(array &$form, FormStateInterface $form_state)
+    {
+        // TODO: Implement submitForm() method.
+    }
+}
---
+++ src/user/user_other.php
@@ -0,0 +1,86 @@
+<?php
+namespace Drupal\nfb_user_portal\user;
+use Drupal\nfb_user_portal\civi_query\query_base;
+class user_other extends user_membership
+{
+
+
+    public $contribution_array;
+    public function get_contribution_array()
+    {return $this->contribution_array;}
+    public $event_array;
+    public function get_event_array()
+    {return $this->event_array;}
+    public function other_page_markup()
+    {
+        $this->civi_contact_set();
+        $this->find_member_records();
+        $this->set_memberhsip_array();
+        $this->find_events();
+        $this->find_contribution();
+    }
+    public function find_contribution()
+    {
+        $civi = new query_base();
+        $civi->mode = "get";
+        $civi->entity = "Contribution";
+        $civi->params = array(
+            'select' => [
+                '*',
+            ],
+            'where' => [
+                ['contact_id', '=', $this->get_user_civi_id()],
+                ['financial_type_id', '=', 1],
+            ],
+            'limit' => 50,
+            'checkPermissions' => FALSE,
+        );
+        $civi->civi_api_v4_query();
+        $result = $civi->get_civi_result();
+        $count = $result->count();
+        $current = 0;
+        $array = [];
+        while($current >= $count )
+        {
+            $contribution = $result->itemat($current);
+            $array[$current]['date'] = $contribution['receive_date'];
+            $array[$current]['amount'] = $contribution['total_amount'];
+            $current++;
+        }
+        $this->contribution_array = $array;
+    }
+    public function find_events()
+    {
+        $civi = new query_base();
+        $civi->mode = "get";
+        $civi->entity = "Participant";
+        $civi->params = array(
+            'select' => [
+                '*',
+                'event.*',
+            ],
+            'join' => [
+                ['Event AS event', 'LEFT', ['event_id', '=', 'event.id']],
+            ],
+            'where' => [
+                ['contact_id', '=', $this->get_user_civi_id()],
+            ],
+            'limit' => 50,
+        );
+        $civi->civi_api_v4_query();
+        $result = $civi->get_civi_result();
+        $count = $result->count();
+        $current = 0;
+        $array = [];
+        while($current >= $count)
+        {
+            $event = $result->itemat($current);
+            $array[$current]['title'] = $event['event.title'];
+            $array[$current]['start_date'] = $event['event.start_date'];
+            $array[$current]['end_date'] = $event['event.end_date'];
+            $current++;
+        }
+        $this->event_array =  $array;
+    }
+
+}
