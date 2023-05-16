9daf040b58745223c1f46d204050290a847842f4
sconnell <sconnell@nfb.org>
2023-05-16 16:29:53 -0400

    UX nonsense

--- src/html_builder/fq_markup_builder.php
+++ src/html_builder/fq_markup_builder.php
@@ -1,1 +1,8 @@
 <?php
+Namespace Drupal\nfb_user_portal\html_builder;
+use Drupal\nfb_user_portal\civi_query\query_base;
+use Drupal\nfb_user_portal\user\user_other;
+class fq_markup_builder extends  other_markup
+{
+
+}
