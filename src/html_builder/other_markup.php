0444071693cb85560cd6e9d43f289f936c3e67b5
sconnell <sconnell@nfb.org>
2023-05-16 14:54:08 -0400

    UX nonsense

---
+++ src/html_builder/other_markup.php
@@ -0,0 +1,7 @@
+<?php
+Namespace Drupal\nfb_user_portal\html_builder;
+use Drupal\nfb_user_portal\civi_query\query_base;
+use Drupal\nfb_user_portal\user\user_other;
+class other_markup extends  memberhisp_markup {
+
+}
