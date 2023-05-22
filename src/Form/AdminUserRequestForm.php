43a6f4c40a30534b98e49b98888778938040d584
sconnell <sconnell@nfb.org>
2023-05-22 09:02:03 -0400

    UX nonsense

--- src/Form/AdminUserRequestForm.php
+++ src/Form/AdminUserRequestForm.php
@@ -2,18 +2,18 @@
 Namespace Drupal\nfb_user_portal\Form;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
-class FaqTabForm extends FormBase
+class AdminUserRequestForm extends FormBase
 {
     public function getFormId()
     {
         // TODO: Implement getFormId() method.
     }
-    public function buildForm(array $form, FormStateInterface $form_state)
-    {
-        // TODO: Implement buildForm() method.
-    }
     public function submitForm(array &$form, FormStateInterface $form_state)
     {
         // TODO: Implement submitForm() method.
+    }
+    public function buildForm(array $form, FormStateInterface $form_state)
+    {
+        // TODO: Implement buildForm() method.
     }
 }
