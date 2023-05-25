b2a51be887cc98a16e49dd33a43eda2ab9758139
sconnell <sconnell@nfb.org>
2023-05-25 09:54:59 -0400

    UX nonsense

--- src/Form/AdminCompleteRequestForm.php
+++ src/Form/AdminCompleteRequestForm.php
@@ -1,1 +1,19 @@
 <?php
+Namespace Drupal\nfb_user_portal\Form;
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+class AdminCompleteRequestForm extends FormBase
+{
+    public function getFormId()
+    {
+       return "nfb_user_admin_complete";
+    }
+    public function buildForm(array $form, FormStateInterface $form_state)
+    {
+        // TODO: Implement buildForm() method.
+    }
+    public function submitForm(array &$form, FormStateInterface $form_state)
+    {
+        // TODO: Implement submitForm() method.
+    }
+}
