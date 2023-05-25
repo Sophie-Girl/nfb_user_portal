6afc344e254a41cdad3fde9a882519b8d4716b89
sconnell <sconnell@nfb.org>
2023-05-25 16:21:47 -0400

    UX nonsense

--- src/Form/OtherInfoForm.php
+++ src/Form/OtherInfoForm.php
@@ -13,9 +13,11 @@ class OtherInfoForm extends FormBase
     public function buildForm(array $form, FormStateInterface $form_state)
     {
        $builder =  new other_markup();
+       $builder->user_data->civi_contact_set();
         $form['other_info'] = array(
             '#type' => "item",
-            '#markup' => "<p>This are is under construction.</p>",//$builder->create_other_markup(),
+            '#markup' => "<p>This are is under construction.</p>
+        <p class='hidden_val' id='member_name'>".$builder->user_data->get_first_name()." ".$this->builder->get_last_name()."</p>",//$builder->create_other_markup(),
             '#allowed_tags' => ['div','span', 'br', 'h2','label','table','thead', 'th', 'td', 'input', 'form', 'select', 'a', 'option', 'button', 'tr', 'p'],
         );
         $form['#attached']['library'][] = 'nfb_user_portal/up-other';
