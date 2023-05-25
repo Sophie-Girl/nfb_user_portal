c76575a1d6e0f287cdb2305f585839b8023096b6
sconnell <sconnell@nfb.org>
2023-05-25 16:13:27 -0400

    UX nonsense

--- src/Form/MemberAccountForm.php
+++ src/Form/MemberAccountForm.php
@@ -16,8 +16,7 @@ class MemberAccountForm extends FormBase
     public function buildForm(array $form, FormStateInterface $form_state)
     {
         $page_builder = new core_markup();
-        $page_builder->user_data->set_user_data();
-        $page_builder->user_data->set_civi_useR_data();
+        $page_builder->user_data->civi_contact_set();
         $form['vals'] = array(
           '#type' => 'item',
           '#markup' => "<p class='hidden_val' id='yoshi'>".\Drupal::currentUser()->getAccountName()."</p>
