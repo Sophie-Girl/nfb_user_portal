f2cec0440c3fd4ccbe8f69c831bf26d26159608d
sconnell <sconnell@nfb.org>
2023-06-06 11:50:20 -0400

    sql fixes

--- src/html_builder/Admin/USer_request_table.php
+++ src/html_builder/Admin/USer_request_table.php
@@ -249,6 +249,7 @@ or review an issue with a potential account.</p>
 
     public function query_switch()
     {
+        \Drupal::logger("name_filter_check")->notice("name filter".$this->get_name_filter());
         if($this->get_name_filter() == " ")
         {
             $name = false;
@@ -256,6 +257,7 @@ or review an issue with a potential account.</p>
         else{
             $name = true;
         }
+        \Drupal::logger("email_filter_check")->notice("email filter".$this->get_email_filter());
         if($this->get_email_filter() == " ")
         {
             $email = false;
@@ -263,6 +265,7 @@ or review an issue with a potential account.</p>
         else{
             $email = true;
         }
+        \Drupal::logger("status_filter_check")->notice("status filter".$this->get_status_filter());
         if($this->get_status_filter() == " ")
         {
             $status = false;
