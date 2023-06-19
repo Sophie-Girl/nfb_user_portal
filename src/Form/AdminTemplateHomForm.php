384bb81ed1ec3f4ea2629cc0855cd23468b7fcfb
sconnell <sconnell@nfb.org>
2023-06-19 08:56:25 -0400

    sql fixes

---
+++ src/Form/AdminTemplateHomForm.php
@@ -0,0 +1,21 @@
+<?php
+Namespace Drupal\nfb_user_portal\Form;
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\nfb_user_portal\civi_query\query_base;
+class AdminTemplateHomForm extends FormBase
+{
+    public function getFormId()
+    {
+        // TODO: Implement getFormId() method.
+    }
+
+    public function buildForm(array $form, FormStateInterface $form_state)
+    {
+        // TODO: Implement buildForm() method.
+    }
+    public function submitForm(array &$form, FormStateInterface $form_state)
+    {
+        // TODO: Implement submitForm() method.
+    }
+}
