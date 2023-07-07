4cec17fab5aa3a73e522e8b907a9d9b83a589baa
sconnell <sconnell@nfb.org>
2023-07-07 16:01:42 -0400

    sql fixes

--- src/html_builder/Admin/edit_create_contnet.php
+++ src/html_builder/Admin/edit_create_contnet.php
@@ -1,8 +1,10 @@
 <?php
 Namespace Drupal\nfb_user_portal\html_builder\Admin;
+use Drupal\Core\Form\FormStateInterface;
+
 class edit_create_contnet
 {
-    public function build_form_array()
+    public function build_form_array(&$form, FormStateInterface $form_state)
     {
 
     }
