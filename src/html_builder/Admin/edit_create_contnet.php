389b24096b420e60116537d1f914bf25284fa445
sconnell <sconnell@nfb.org>
2023-07-07 16:10:05 -0400

    sql fixes

--- src/html_builder/Admin/edit_create_contnet.php
+++ src/html_builder/Admin/edit_create_contnet.php
@@ -6,6 +6,15 @@ class edit_create_contnet
 {
     public function build_form_array(&$form, FormStateInterface $form_state)
     {
-
+        $form['markup_type']  = array(
+            '#type' => "select",
+            '#title' => "Filter By Markup Type",
+            '#options' => array(
+                'intro_text' => "Intro Text",
+                "member_benefit" => "Member Benefit",
+                'faq' => "FAQ",
+                'content_text' => "Content Text"
+            ),
+        );
     }
 }
