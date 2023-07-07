ff825214b9e6567f8f821ec33a17dc4f4c54053b
sconnell <sconnell@nfb.org>
2023-07-07 14:12:50 -0400

    sql fixes

--- src/html_builder/Admin/markup_talbe_create.php
+++ src/html_builder/Admin/markup_talbe_create.php
@@ -95,10 +95,12 @@ class markup_talbe_create extends markup_table_edit {
     }
     public function sql_query()
     {
+
         $this->sql = new User_request_queries();
         $query = "select * from nfb_user_portal_content order by active desc";
         $key = "cid";
         $this->sql->select_query($query, $key);
+        $this->header_build();
         foreach ($this->sql->get_result() as $content)
         {
             $content = get_object_vars($content);
@@ -120,6 +122,7 @@ class markup_talbe_create extends markup_table_edit {
                 $this->build_table_row($content, $array_markup);
             }
             }
+        $this->end_table();
 
     }
     public function query_setup(FormStateInterface  $form_state)
