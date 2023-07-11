67ea713101b4639f05c2cebe31a92cbd4634b037
sconnell <sconnell@nfb.org>
2023-07-11 08:12:47 -0400

    sql fixes

---
+++ js/admin-content-edit.js
@@ -0,0 +1,9 @@
+(function ($, Drupal) {
+    Drupal.behaviors.up_other = {
+        attach: function (context, settings) {
+            window.onload = function () {
+
+
+            }
+        }
+    }})(jQuery, Drupal);
---
+++ src/Controller/AdminContentController.php
@@ -0,0 +1,8 @@
+<?php
+Namespace Drupal\nfb_user_portal\Controller;
+use Drupal\Core\Controller\ControllerBase;
+
+class AdminContentController extends ControllerBase
+{
+
+}
