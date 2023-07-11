6a1bc91e5a0393d5193cc7690a75601aa32eb74f
sconnell <sconnell@nfb.org>
2023-07-11 16:16:31 -0400

    sql fixes

--- src/Form/AdminTemplateCreateForm.php
+++ src/Form/AdminTemplateCreateForm.php
@@ -3,15 +3,21 @@ Namespace Drupal\nfb_user_portal\Form;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\nfb_user_portal\civi_query\query_base;
+use Drupal\nfb_user_portal\html_builder\Admin\edit_create_contnet;
+use Drupal\nfb_user_portal\SQL\admin\User_request_queries;
 class AdminTemplateCreateForm extends FormBase
 {
+
     public function getFormId()
     {
         return "template_member_creation";
     }
-    public function buildForm(array $form, FormStateInterface $form_state)
+    public function buildForm(array $form, FormStateInterface $form_state, $content=1)
     {
-        // TODO: Implement buildForm() method.
+        $user_request_queries = new User_request_queries();
+        $factory =  new edit_create_contnet($user_request_queries);
+        $factory->build_form_array($form, $form_state, $content);
+        return $form;
     }
     public function submitForm(array &$form, FormStateInterface $form_state)
     {
