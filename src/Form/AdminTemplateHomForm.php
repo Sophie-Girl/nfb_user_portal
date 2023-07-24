a5ceb3390e9001ca5f19e9de7875e13c4dd0c5b3
sconnell <sconnell@nfb.org>
2023-07-24 09:33:02 -0400

    sql fixes

--- src/Form/AdminTemplateHomForm.php
+++ src/Form/AdminTemplateHomForm.php
@@ -3,6 +3,8 @@ Namespace Drupal\nfb_user_portal\Form;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\nfb_user_portal\civi_query\query_base;
+use Drupal\nfb_user_portal\html_builder\Admin\markup_talbe_create;
+
 class AdminTemplateHomForm extends FormBase
 {
     public function getFormId()
@@ -10,9 +12,11 @@ class AdminTemplateHomForm extends FormBase
        return "member_template_text_home";
     }
 
-    public function buildForm(array $form, FormStateInterface $form_state)
+    public function buildForm(array $form, FormStateInterface $form_state,$content = "new" )
     {
-        // TODO: Implement buildForm() method.
+        $factory = new markup_talbe_create();
+        $factory->build_form_array($form, $form_state, $content);
+        return $form;
     }
     public function submitForm(array &$form, FormStateInterface $form_state)
     {
