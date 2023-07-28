31ead8c00cf27b6c6e075b368a220035e9944f72
sconnell <sconnell@nfb.org>
2023-07-28 16:34:58 -0400

    sql fixes

---
+++ src/html_builder/member_account.php
@@ -0,0 +1,7 @@
+<?php
+Namespace Drupal\nfb_user_portal\html_builder;
+use Drupal\nfb_user_portal\civi_query\query_base;
+use Drupal\nfb_user_portal\SQL\admin\User_request_queries;
+class member_account {
+
+}
