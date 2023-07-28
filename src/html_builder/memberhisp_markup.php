ccafe452f797ed1cbcff2bf28c5c638b8c814402
sconnell <sconnell@nfb.org>
2023-07-28 11:23:38 -0400

    sql fixes

--- src/html_builder/memberhisp_markup.php
+++ src/html_builder/memberhisp_markup.php
@@ -184,7 +184,6 @@ class memberhisp_markup
         $query = "select * from nfb_user_portal_content where markup_type = 'member_benefit' and tab = 'membership' and active = '0';";
         $sql = new User_request_queries();
         $sql->select_query($query, $key);
-        \Drupal::logger("query_debug")->notice("query_results ".print_r($sql->get_result(), true));
         $this->beneift_array = $sql->get_result();
     }
     public function process_array()
@@ -196,6 +195,7 @@ class memberhisp_markup
             $content = get_object_vars($content);
             $array = json_decode($content['markup']);
             $array = get_object_vars($array);
+            \Drupal::logger("query_debug")->notice("query_results ".print_r($array, true));
             if($array['group'] == "base")
             {
                 $base_benefit[$array['weight']] = array(
