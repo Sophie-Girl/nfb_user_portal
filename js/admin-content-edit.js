63539e389ba230e4f1935d6c1469858127322d44
sconnell <sconnell@nfb.org>
2023-08-15 12:58:09 -0400

    back from vacay

--- js/admin-content-edit.js
+++ js/admin-content-edit.js
@@ -197,7 +197,7 @@
                     document.getElementById('edit-weight').value = document.getElementById('weight_val').innerText;
                     document.getElementById('edit-permanent').value = document.getElementById("perm_val").innerText;
                    var ck = document.getElementsByClassName("cke_editable cke_editable_themed cke_contents_ltr cke_show_borders");
-                    ck.innerHTML = document.getElementById('content-val').innerText;
+                    ck.value= document.getElementById('content-val').innerText;
                 }
 
             }
