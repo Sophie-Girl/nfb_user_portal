31f763f08fb7d803ee1150df769ec855525d731b
sconnell <sconnell@nfb.org>
2023-08-16 12:10:46 -0400

    back from vacay

--- src/html_builder/Admin/markup_talbe_create.php
+++ src/html_builder/Admin/markup_talbe_create.php
@@ -227,7 +227,7 @@ class markup_talbe_create extends markup_table_edit {
     public function build_table_row($content, $markup_array)
     {
         $this->markup = $this->get_markup()."<tr><td>".$content['cid']."</td><td>".$content['markup_type']."</td><td>".$markup_array['title']."</td><td>".$content['tab']."</td>
-        <td>".$content['limiter']."</td><td>".$this->find_civi_entity_title($content)."</td><td>".$this->yes_no($content)."</td><td>".$markup_array['weight']."</td><td><a href='/member_portal/admin/content/".$content['cid']."' role='button' aria-label='Edit '>&nbsp;&nbsp;&nbsp;Edit&nbsp;&nbsp;&nbsp;</a></td></tr>";
+        <td>".$content['limiter']."</td><td>".$this->find_civi_entity_title($content)."</td><td>".$this->yes_no($content)."</td><td>".$markup_array['weight']."</td><td><a href='/member_portal/admin/content/".$content['cid']."' role='button' aria-label='Edit '>Edit</a></td></tr>";
 
     }
     public function yes_no($content)
